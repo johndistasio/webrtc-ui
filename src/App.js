c3ebe5e09166d6612a1bae47a6fa8bcf8d98ef15
John Distasio <jndistasio@gmail.com>
2020-09-20 00:25:21 -0400

    Add react-router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,34 @@
 import React, { useState } from 'react'
+import { BrowserRouter as Router, Route, Redirect, Switch, Link, useParams, useRouteMatch } from 'react-router-dom'
 import './App.css'
 
 function App () {
+  return (
+    <Router>
+      <Switch>
+        <Route path='/' exact component={Launcher}/>
+        <Route path='/call/:call' component={Call}/>
+        <Route path='*'>
+          <Redirect to='/'/>
+        </Route>
+      </Switch>
+    </Router>
+  )
+}
+
+function Launcher () {
+  const { url } = useRouteMatch()
+
+  return (
+    <Link to={`${url}call/test`}>
+      <div id='launcher'>Launcher</div>
+    </Link>
+  )
+}
+
+function Call () {
+  const { call } = useParams()
+  const { url } = useRouteMatch()
   const [clazz, setClazz] = useState('green')
 
   const toggleColor = () => {
@@ -9,11 +36,13 @@ function App () {
   }
 
   return (
-    <div id='app'>
-      <div id='launcher'>Launcher</div>
-      <div id='remote-video' className='video'>Remote Video</div>
+    <div id='call'>
+      <div id='remote-video' className='video'>{call}</div>
       <div id='local-video' className={clazz} onClick={() => { toggleColor() }}>Local Video</div>
       <div id='controls'>
+        <Link to='/'>
+          <span>Back</span>
+        </Link>
         <button title='Start Call'>Start Call</button>
         <button title='Toggle Mute'>Mute</button>
         <button title='Toggle Preview'>Stop Preview</button>
