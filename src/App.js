fc7f750fd7b3806cca13bf11cf46b8a6519067d2
John Distasio <john@sect0r7.net>
2020-09-19 23:35:33 -0400

    Add example hook usage

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './App.css'
 
 function App () {
+  const [clazz, setClazz] = useState('green')
+
+  const toggleColor = () => {
+    setClazz(clazz === 'green' ? 'orange' : 'green')
+  }
+
   return (
-    <div id="app">
-      <div id="launcher">Launcher</div>
-      <div id="remote-video" className="video">Remote Video</div>
-      <div id="local-video" className="video">Local Video</div>
-      <div id="controls">
-        <button title="Start Call">Start Call</button>
-        <button title="Toggle Mute">Mute</button>
-        <button title="Toggle Preview">Stop Preview</button>
-        <button title="End Call">End Call</button>
+    <div id='app'>
+      <div id='launcher'>Launcher</div>
+      <div id='remote-video' className='video'>Remote Video</div>
+      <div id='local-video' className={clazz} onClick={() => { toggleColor() }}>Local Video</div>
+      <div id='controls'>
+        <button title='Start Call'>Start Call</button>
+        <button title='Toggle Mute'>Mute</button>
+        <button title='Toggle Preview'>Stop Preview</button>
+        <button title='End Call'>End Call</button>
       </div>
     </div>
   )
