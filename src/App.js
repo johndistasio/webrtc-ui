1498cbfafbd2fc56a64dc6db2516c93c8cd978d1
John Distasio <jndistasio@gmail.com>
2020-09-19 21:21:06 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="app">
+      <div id="launcher">Launcher</div>
+      <div id="remote-video" class="video">Remote Video</div>
+      <div id="local-video" class="video">Local Video</div>
+      <div id="controls">
+        <button title="Start Call">Start Call</button>
+        <button title="Toggle Mute">Mute</button>
+        <button title="Toggle Preview">Stop Preview</button>
+        <button title="End Call">End Call</button>
+      </div>
     </div>
   );
 }
