b4be50410e46af0a07d80f984367f397a3ecf191
John Distasio <john@sect0r7.net>
2020-09-21 19:54:10 -0400

    Extract Call component

--- src/Call.test.js
+++ src/Call.test.js
@@ -1,16 +1,12 @@
 import React from "react";
 import { render } from "@testing-library/react";
-import App, { Launcher } from "./App";
+import Call from "./Call";
 import { MemoryRouter as Router } from "react-router-dom";
 
-test("App component renders", () => {
-  render(<App />);
-});
-
-test("Launcher component renders", () => {
+test("Call component renders", () => {
   render(
     <Router>
-      <Launcher />
+      <Call />
     </Router>
   );
 });
