ebba427f57d66b8f7df01879badae22db4cac608
John Distasio <john@sect0r7.net>
2020-09-21 19:32:03 -0400

    Add some smoke tests for existing components

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,24 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, fireEvent, screen } from "@testing-library/react";
+import App, { Launcher, Call } from "./App";
+import { MemoryRouter as Router } from "react-router-dom";
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test("App component renders", () => {
+  render(<App />);
+});
+
+test("Launcher component renders", () => {
+  render(
+    <Router>
+      <Launcher />
+    </Router>
+  );
+});
+
+test("Call component renders", () => {
+  render(
+    <Router>
+      <Call />
+    </Router>
+  );
 });
