c216f12aede45a6ba74c8016866393ea3385ea5f
John Distasio <john@sect0r7.net>
2020-09-21 23:04:03 -0400

    Start noodling on call join

--- src/Call.js
+++ src/Call.js
@@ -1,15 +1,66 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
 import "./Call.css";
 
 function Call() {
-  const { call } = useParams();
+  const { call } = useParams("");
+  const [joined, setJoined] = useState(false);
+  const [muted, setMuted] = useState(false);
+  const [session, setSession] = useState(null);
+
+  useEffect(() => {
+    // TODO parameterize URL
+    fetch(`http://localhost:8080/call/${call}`)
+      .then((response) => {
+        if (response.status === 409) {
+          // TODO handle error condition
+          return;
+        }
+
+        if (!response.ok) {
+          // TODO handle error condition
+          return;
+        }
+
+        // TODO parameterize header name
+        let id = response.headers.get("X-Signal-Session");
+
+        if (id === null) {
+          // TODO handle error condition
+          return;
+        }
+
+        setSession(id);
+        setJoined(true);
+      })
+      .then((id) => {
+        console.log(`Joined call ${call} as ${id}`);
+      })
+      .catch((error) => {
+        setSession(null);
+        setJoined(false);
+      });
+
+    return () => console.log("Call cleanup");
+  }, [call]);
+
+  const joinCall = () => console.log("join call");
+
+  const mute = () => {
+    setMuted(!muted);
+    console.log("mute");
+  };
 
   return (
     <div className="call">
-      <CallScreen callId={call} />
+      <CallScreen callId={`Call: ${call} Session: ${session}`} />
       <CallPreview />
-      <CallControls />
+      <CallControls
+        disabled={!joined}
+        onJoinCall={joinCall}
+        muted={muted}
+        onMute={mute}
+      />
     </div>
   );
 }
@@ -30,16 +81,27 @@ function CallPreview() {
   );
 }
 
-function CallControls() {
+function CallControls(props) {
   return (
     <div className="call-controls">
       <Link to="/">
-        <span>Back</span>
+        <button title="Back">Back</button>
       </Link>
-      <button title="Start Call">Start Call</button>
-      <button title="Toggle Mute">Mute</button>
-      <button title="Toggle Preview">Stop Preview</button>
-      <button title="End Call">End Call</button>
+      <button disabled={props.disabled} onClick={() => props.onJoinCall()}>
+        Join Call
+      </button>
+      <button disabled={props.disabled} onClick={() => props.onMute()}>
+        {props.muted ? "Mute" : "Unmute"}
+      </button>
+      <button disabled={props.disabled} title="Toggle Camera">
+        Stop Video
+      </button>
+      <button disabled={props.disabled} title="Leave Call">
+        Leave Call
+      </button>
+      <button disabled={props.disabled} title="Toggle Preview">
+        Stop Preview
+      </button>
     </div>
   );
 }
