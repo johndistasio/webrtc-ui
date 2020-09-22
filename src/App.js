257a2b352fb8ce6f4dbbb7e51ac638882a24c691
John Distasio <jndistasio@gmail.com>
2020-09-21 20:07:44 -0400

    Extract Launcher component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
-import React, { useState } from "react";
+import React from "react";
 import {
   BrowserRouter as Router,
   Route,
   Redirect,
   Switch,
-  Link,
 } from "react-router-dom";
-import { Button, Card, OutlinedInput } from "@material-ui/core";
-import CallIcon from "@material-ui/icons/Call";
 import "./App.css";
 import Call from "./Call";
+import Launcher from "./Launcher";
 
 function App() {
   return (
@@ -29,66 +27,6 @@ function App() {
   );
 }
 
-function getRandomInt(max) {
-  return Math.floor(Math.random() * Math.floor(max));
-}
-
-function Launcher() {
-  // TODO use a friendlier, memorable format for random call IDs
-  const randomCall = () => `call-${getRandomInt(9999)}-${getRandomInt(9999)}`;
-
-  const [call, setCall] = useState(randomCall());
-
-  return (
-    <Card id="launcher">
-      <div id="launcher-input">
-        <OutlinedInput
-          placeholder="Enter a Call ID"
-          value={call}
-          onChange={(e) => setCall(e.target.value)}
-        />
-        <Button
-          variant="contained"
-          color="primary"
-          disabled={call === ""}
-          startIcon={<CallIcon />}
-          component={Link}
-          to={`/call/${call}`}
-        >
-          Join
-        </Button>
-      </div>
-      <div id="launcher-controls">
-        <Button
-          variant="outlined"
-          color="primary"
-          onClick={() => setCall(randomCall())}
-        >
-          Random
-        </Button>
-        <Button
-          variant="outlined"
-          color="secondary"
-          disabled={call === ""}
-          onClick={() => setCall("")}
-        >
-          Clear
-        </Button>
-      </div>
-      <div id="launcher-extra">
-        <ul>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-          <li>
-            <Link to="/terms">Terms of Service</Link>
-          </li>
-        </ul>
-      </div>
-    </Card>
-  );
-}
-
 function About() {
   return <div id="about">About</div>;
 }
@@ -98,4 +36,3 @@ function TermsOfService() {
 }
 
 export default App;
-export { Launcher, Call };
--- src/Launcher.test.js
+++ src/Launcher.test.js
@@ -1,8 +1,12 @@
 import React from "react";
 import { render } from "@testing-library/react";
-import App from "./App";
 import { MemoryRouter as Router } from "react-router-dom";
+import Launcher from "./Launcher";
 
-test("App component renders", () => {
-  render(<App />);
+test("Launcher component renders", () => {
+  render(
+    <Router>
+      <Launcher />
+    </Router>
+  );
 });
